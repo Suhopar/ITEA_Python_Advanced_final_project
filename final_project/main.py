a26dfb7b42ee330e7272296973b03b43af199518
Suhopar <ysysuhopar@gmail.com>
2019-09-26 18:17:50 +0300

    upd 3 p

---
+++ final_project/main.py
@@ -0,0 +1,263 @@
+import telebot
+from bot.config import TOKEN, START_KEYBOARD, MESSAGE_NOTIFICATION
+from models.cats_and_products import Texts, Category, Cart, Product, OrdersHistory
+from mongoengine import connect
+from models.user_model import User
+from bson import ObjectId
+from telebot.types import (
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+    ReplyKeyboardMarkup
+)
+
+connect('bot_shop')
+bot = telebot.TeleBot(TOKEN)
+
+
+class Storage:
+    language = ''
+    user_id = ''
+
+
+@bot.message_handler(commands=['start'])
+def language_or_greetings(message):
+    if User.objects(user_id=message.from_user.id):
+        language = User.objects.get(user_id=message.from_user.id).get_user_language
+        start_keyboard = START_KEYBOARD[language]
+        s = u'\U0000270C'
+        hello = s + Texts.objects.get(title='Greetings', language=language).text + f', {message.chat.first_name}'
+        kb = ReplyKeyboardMarkup(resize_keyboard=True)
+        kb.add(*start_keyboard.values())
+        bot.send_message(message.chat.id, hello, reply_markup=kb)
+
+    else:
+        User.get_or_create_user(message, 'uk')
+        kb = ReplyKeyboardMarkup(resize_keyboard=True)
+        kb.add(START_KEYBOARD['uk'].values())
+
+
+@bot.message_handler(func=lambda m: m.text == START_KEYBOARD[User.objects.get(
+                                    user_id=m.from_user.id).get_user_language]['category'])
+def show_category(message):
+    print("Category")
+    if User.objects.get(user_id=message.from_user.id):
+        language = User.objects.get(user_id=message.from_user.id).get_user_language
+    else:
+        language = 'uk'
+    inline_kb = telebot.types.InlineKeyboardMarkup(row_width=2)
+    buttons_list = []
+    for i in Category.objects:
+        callback_data = 'category_' + str(i.id)
+        if i.is_parent:
+            callback_data = 'subcategory_' + str(i.id)
+            buttons_list.append(
+                telebot.types.InlineKeyboardButton(text=i.title + " >>", callback_data=callback_data))
+        # else:
+        #     buttons_list.append(
+        #         telebot.types.InlineKeyboardButton(text=i.title, callback_data=callback_data))
+    inline_kb.add(*buttons_list)
+    bot.send_message(chat_id=message.chat.id,
+                     text=START_KEYBOARD[language]['category'],
+                     reply_markup=inline_kb, )
+
+
+@bot.callback_query_handler(func=lambda call: call.data.split('_')[0] == 'subcategory')
+def sub_cat(call):
+    print(call.id)
+    bot.edit_message_reply_markup(call.message.chat.id, call.message.message_id)
+    subcats_kb = telebot.types.InlineKeyboardMarkup()
+    subcats_buttons = []
+    subcats = Category.objects.get(id=call.data.split('_')[1])
+    for i in subcats.sub_categories:
+        # if i.is_parent:
+        #     callback_data = 'subcategory_' + str(i.id)
+        # else:
+        callback_data = 'category_' + str(i.id)
+
+        subcats_buttons.append(
+            telebot.types.InlineKeyboardButton(text=i.title,
+                                               callback_data=callback_data)
+        )
+    subcats_buttons.append(
+        telebot.types.InlineKeyboardButton(text="<<",
+                                           callback_data="<<")
+    )
+    subcats_kb.add(*subcats_buttons)
+    bot.send_message(call.from_user.id,
+                     text='sub category',
+                     reply_markup=subcats_kb)
+
+
+@bot.callback_query_handler(func=lambda call: call.data == '<<')
+def beck_to_cat(call):
+    bot.edit_message_reply_markup(call.message.chat.id, call.message.message_id)
+    if User.objects.get(user_id=call.message.chat.id):
+        language = User.objects.get(user_id=call.message.chat.id).get_user_language
+    else:
+        language = 'uk'
+    inline_kb = telebot.types.InlineKeyboardMarkup(row_width=2)
+    buttons_list = []
+    for i in Category.objects:
+        callback_data = 'category_' + str(i.id)
+        if i.is_parent:
+            callback_data = 'subcategory_' + str(i.id)
+            buttons_list.append(
+                telebot.types.InlineKeyboardButton(text=i.title + " >>", callback_data=callback_data))
+        # else:
+        #     buttons_list.append(
+        #         telebot.types.InlineKeyboardButton(text=i.title, callback_data=callback_data))
+    inline_kb.add(*buttons_list)
+    bot.send_message(chat_id=call.message.chat.id,
+                     text=START_KEYBOARD[language]['category'],
+                     reply_markup=inline_kb)
+
+
+
+@bot.callback_query_handler(func=lambda call: call.data.split('_')[0] == 'category')
+def show_product(call):
+    print(call.data.split('_')[1])
+    cat = Category.objects.filter(id=call.data.split('_')[1]).first()
+    print(call)
+    products = cat.category_products
+    print(products)
+    language = User.objects.get(user_id=call.from_user.id).get_user_language
+    cart_lang = MESSAGE_NOTIFICATION[language]['in_cart']
+    info_lang = MESSAGE_NOTIFICATION[language]['details']
+
+    if not products:
+        bot.send_message(call.message.chat.id, MESSAGE_NOTIFICATION[language]['no_products_in_category'])
+
+    for p in products:
+        products_kb = telebot.types.InlineKeyboardMarkup(row_width=2)
+        products_kb.add(telebot.types.InlineKeyboardButton(text=cart_lang,
+                                                           callback_data='addtocart_' + str(p.id)),
+                        telebot.types.InlineKeyboardButton(text=info_lang,
+                                                           callback_data='product_' + str(p.id)))
+
+        # bot.send_photo(call.message.chat.id, p.image.get(),
+        #                caption=p.title + p.description, reply_markup=products_kb)
+        bot.send_message(call.message.chat.id, text=p.title, reply_markup=products_kb) #  +'\n'+ p.description
+
+
+@bot.callback_query_handler(func=lambda call: call.data.split('_')[0] == 'addtocart')
+def add_to_cart(call):
+    Cart.create_or_append_to_cart(product_id=call.data.split('_')[1],
+                                  user_id=call.message.chat.id)
+    cart = Cart.objects.all().first()
+    print("add_to_cart - ", cart.products)
+
+
+@bot.callback_query_handler(func=lambda call: call.data.split('_')[0] == 'product')
+def show_info_product(call):
+    product_info = Product.objects.get(id=call.data.split('_')[1]).get_product_info
+    language = User.objects.get(user_id=call.message.from_user.id).get_user_language
+    bot.send_message(call.message.chat.id, f"{MESSAGE_NOTIFICATION[language]['price']} - {product_info['price']}"
+                                           f"\n{MESSAGE_NOTIFICATION[language]['clothing_size']}"
+                                           f" - {product_info['clothing_size']}"
+                                           f"\n{MESSAGE_NOTIFICATION[language]['quantity']}"
+                                           f" - {product_info['quantity']}")
+
+
+@bot.message_handler(func=lambda m: m.text == START_KEYBOARD[User.objects.get(
+                                    user_id=m.from_user.id).get_user_language]['cart'])
+def show_cart(message):
+    current_user = User.objects.get(user_id=message.chat.id)
+    cart = Cart.objects.filter(user=current_user, is_archived=False).first()
+    language = User.objects.get(user_id=message.from_user.id).get_user_language
+    if not cart:
+        bot.send_message(message.chat.id, MESSAGE_NOTIFICATION[language]['cart_empty'])
+        return
+
+    if not cart.products:
+        bot.send_message(message.chat.id, MESSAGE_NOTIFICATION[language]['cart_empty'])
+        return
+
+    for product in cart.products:
+        remove_kb = InlineKeyboardMarkup()
+        remove_button = InlineKeyboardButton(text=MESSAGE_NOTIFICATION[language]['remove_product_from_cart'],
+                                             callback_data='rmproduct_' + str(product.id))
+        remove_kb.add(remove_button)
+        bot.send_message(message.chat.id, product.title,
+                         reply_markup=remove_kb)
+
+    submit_kb = InlineKeyboardMarkup()
+    submit_button = InlineKeyboardButton(
+        text=MESSAGE_NOTIFICATION[language]['сheckout_order'],
+        callback_data='submit'
+    )
+    submit_kb.add(submit_button)
+    bot.send_message(message.chat.id,
+                     MESSAGE_NOTIFICATION[User.objects.get(
+                         user_id=message.from_user.id).get_user_language]['confirm_your_order'], reply_markup=submit_kb)
+
+
+@bot.callback_query_handler(func=lambda call: call.data.split('_')[0] == 'rmproduct')
+def rm_product_from_cart(call):
+    current_user = User.objects.get(user_id=call.message.chat.id)
+    cart = Cart.objects.get(user=current_user)
+    cart.update(pull__products=ObjectId(call.data.split('_')[1]))
+    bot.delete_message(call.message.chat.id, call.message.message_id)
+
+
+@bot.callback_query_handler(func=lambda call: call.data.split('_')[0] == 'submit')
+def submit_cart(call):
+    current_user = User.objects.get(user_id=call.message.chat.id)
+    cart = Cart.objects.filter(user=current_user, is_archived=False).first()
+    cart.is_archived = True
+
+    order_history = OrdersHistory.get_or_create(current_user)
+    order_history.orders.append(cart)
+    bot.send_message(call.message.chat.id, MESSAGE_NOTIFICATION[current_user.get_user_language]['thank_for_order'])
+    cart.save()
+    order_history.save()
+
+
+@bot.message_handler(func=lambda m: m.text == START_KEYBOARD[User.objects.get(
+                                    user_id=m.from_user.id).get_user_language]['cart_archive'])
+def show_arh(message):
+    list_product = (OrdersHistory.objects.filter(user=User.objects.get(user_id=message.chat.id)).first()).get_orders
+    for i in range(0, len(list_product)):
+        p = list_product[i].products
+        for i in range(0, len(p)):
+            bot.send_message(message.chat.id, p[i].title)
+
+
+@bot.message_handler(func=lambda m: m.text == START_KEYBOARD[User.objects.get(
+                                    user_id=m.from_user.id).get_user_language]['language'])
+def language(message):
+    products_kb = telebot.types.InlineKeyboardMarkup(row_width=2)
+    products_kb.add(telebot.types.InlineKeyboardButton(text='uk',
+                                                       callback_data='language_' + 'uk'),
+                    telebot.types.InlineKeyboardButton(text='ru',
+                                                       callback_data='language_' + 'ru'))
+    bot.send_message(message.chat.id, text='language', reply_markup=products_kb)
+
+
+@bot.callback_query_handler(func=lambda call: call.data.split('_')[0] == 'language')
+def lang_upd(call):
+    lang = call.data.split('_')[1]
+    User.objects(user_id=call.message.chat.id).update(language=lang)
+    bot.send_message(call.message.chat.id, MESSAGE_NOTIFICATION[lang]['enter_start'])
+
+
+@bot.message_handler(func=lambda m: m.text == START_KEYBOARD[User.objects.get(
+                                    user_id=m.from_user.id).get_user_language]['latest_news'])
+def latest_news(message):
+    print("Latest news")
+    bot.send_message(message.chat.id, Texts.objects.get(title=START_KEYBOARD['uk']['latest_news'],
+                                                        language=User.objects.get(
+                                                            user_id=message.from_user.id).get_user_language).text)
+
+
+@bot.message_handler(func=lambda m: m.text == START_KEYBOARD[User.objects.get(
+                                    user_id=m.from_user.id).get_user_language]['buyer_information'])
+def buyer_information(message):
+    print("Buyer Information")
+    bot.send_message(message.chat.id, Texts.objects.get(title=START_KEYBOARD['uk']['buyer_information'],
+                                                        language=User.objects.get(
+                                                            user_id=message.from_user.id).get_user_language).text)
+
+
+if __name__ == '__main__':
+    print("Bot started")
+    bot.polling()
---
+++ final_project/user_model_seeder.py
@@ -0,0 +1,145 @@
+import random
+import string
+from models.cats_and_products import Category, Product, Texts
+from mongoengine import connect
+
+random_bool = (True, False)
+
+
+def random_string(str_len=20):
+    letters = string.ascii_letters
+    return ''.join(random.choice(letters) for i in range(str_len))
+
+
+def seed_and_get_categories(list_cat, language):
+    category_list = []
+    for i in range(0, len(list_cat)):
+        cat = Category(title=list_cat[i], language=language).save()
+        category_list.append(cat)
+    return category_list
+
+
+def seed_and_get_categories_2():
+    category_list = ["Outerwear", "T-shirts and shirts", "Pants", "Footwear", "Other clothes"]
+    language = 'uk'
+    l = []
+    for i in range(0, len(category_list)):
+        l.append(Category(title=str(category_list[i]), language=language).save())
+    return l
+
+
+def seed_and_cat_sub_cat():  # seed_sub_cat
+    cats = seed_and_get_categories_2()
+    language = 'uk'
+    # category_list = ["Outerwear", "T-shirts and shirts", "Pants", "Footwear", "Other clothes"]
+    outerwear = ["Jackets"]
+    t_shirts_and_shirts = ["Blouse", "T-shirt", "Shirts"]
+    pants = ["Sports Pants", "Casual Pants", "Shorts"]
+    footwear = ["Sneakers", "Shoes"]
+    other_clothes = ["Dress", "Pajamas", "Overalls", "Spacesuit"]
+
+    for obj_cat in Category.objects:
+        if obj_cat.title == 'Outerwear':
+            cat_obj = Category.objects.get(title="Outerwear", language=language)
+            cat_obj.sub_categories = seed_and_get_categories(outerwear, language)
+            cat_obj.save()
+        elif obj_cat.title == 'T-shirts and shirts':
+            cat_obj = Category.objects.get(title="T-shirts and shirts", language=language)
+            cat_obj.sub_categories = seed_and_get_categories(t_shirts_and_shirts, language)
+            cat_obj.save()
+        elif obj_cat.title == 'Pants':
+            cat_obj = Category.objects.get(title="Pants", language=language)
+            cat_obj.sub_categories = seed_and_get_categories(pants, language)
+            cat_obj.save()
+        elif obj_cat.title == 'Footwear':
+            cat_obj = Category.objects.get(title="Footwear", language=language)
+            cat_obj.sub_categories = seed_and_get_categories(footwear, language)
+            cat_obj.save()
+        elif obj_cat.title == 'Other clothes':
+            cat_obj = Category.objects.get(title="Other clothes", language=language)
+            cat_obj.sub_categories = seed_and_get_categories(other_clothes, language)
+            cat_obj.save()
+
+
+def seed_products():
+    all_cat = ["Jackets", "Blouse", "T-shirt", "Shirts", "Sports Pants", "Casual Pants", "Shorts", "Sneakers",
+               "Shoes", "Dress", "Pajamas", "Overalls", "Spacesuit"]
+    clothing_size_list = ["XS", "S", "M", "L", "XL", "XXL"]
+    for i in all_cat:
+        for e in range(1, 4):
+            cat_title = Category.objects.get(title=i)
+            print(i)
+            product = dict(
+                title=i + ' ' + str(e),
+                description='Very cool ' + i + str(e),
+                price=random.randint(1000, 100 * 1000),
+                quantity=random.randint(0, 100),
+                is_available=random.choice(random_bool),
+                is_discount=random.choice(random_bool),
+                clothing_size=random.choice(clothing_size_list),
+                category=Category.objects.get(title=i)
+
+            )
+            prod = Product(**product).save()
+            # url = r'images/'+i+'.jpg'
+            # marmot_photo = open(r'images\\'+i+'.jpg', 'rb')
+            # prod.photo.put(marmot_photo, content_type='image/jpeg')
+            # prod.save()
+
+
+def seed_products_with_image():
+    products = Product.objects.all()
+    all_cat = ["Jackets", "Blouse", "T-shirt", "Shirts", "Sports Pants", "Casual Pants", "Shorts", "Sneakers",
+               "Shoes", "Dress", "Pajamas", "Overalls", "Spacesuit"]
+    for i in products:
+        title = i.title
+
+        with open(r'images\test.png', 'rb') as image:
+            i.image.put(image)
+            i.save()
+
+
+def seed_texts():
+    title_list = ["Greetings", "Greetings", "Latest news", "Latest news", "Buyer Information", "Buyer Information"]
+    text_list = ["Good day", "Доброго времени суток",
+                 "A batch of T-shirts with a print of a cowboy-sloth riding a pig has been stolen",
+                 "Была украдена партия футболок с принтом ленивца-ковбоя на свинье",
+                 "There will be no discounts", "Не будет никаких скидок"]
+    l = ''
+    for i in range(6):
+        if i == 0:
+            l = 'uk'
+        elif i % 2:
+            l = 'ru'
+        else:
+            l = 'uk'
+        text = dict(
+            title=title_list[i],
+            text=text_list[i],
+            language=l
+        )
+        Texts(**text).save()
+
+
+if __name__ == '__main__':
+
+    pass
+    connect('bot_shop')
+    # seed_texts()
+    # seed_and_cat_sub_cat()
+    seed_products()
+
+    # ----------
+    # seed_products_with_image()
+    # cats = seed_and_get_categories_2()
+    # seed_products(20, cats)
+    # text = dict(
+    #     title='Greetings',
+    #     text='Доброго времени суток',
+    #     language='ru'
+    # )
+    # Texts(**text).save()
+    # cats = seed_and_get_categories_2()
+    # cat_obj = Category.objects.get(title="sneakers")
+    # cat_obj.sub_categories = cats
+    # print(cat_obj.save())
