b249b1bcd93eceea5b64c7c3a79cc52e43f243c5
Suhopar <ysysuhopar@gmail.com>
2019-09-30 15:23:17 +0300

    upd 5 p

--- final_project/main_webhook.py
+++ final_project/main_webhook.py
@@ -10,12 +10,44 @@ from telebot.types import (
     ReplyKeyboardMarkup
 )
 
+#sudo apt-get install openssl
+#openssl genrsa -out webhook_pkey.pem 2048
+#openssl req -new -x509 -days 3650 -key webhook_pkey.pem -out webhook_cert.pem
+import time
+from flask import Flask, request, abort
 # from mongoengine import register_connection
 #
 # register_connection(alias=None, db='bot_shop', host='35.224.157.246', port='27017')
 
 connect('bot_shop')
 bot = telebot.TeleBot(TOKEN)
+
+API_TOKEN = TOKEN
+
+WEBHOOK_HOST = '34.67.34.201'
+WEBHOOK_PORT = 80  # 443, 80, 88 or 8443 (port need to be 'open')
+WEBHOOK_LISTEN = '0.0.0.0'  # In some VPS you may need to put here the IP addr
+
+WEBHOOK_SSL_CERT = './webhook_cert.pem'  # Path to the ssl certificate
+WEBHOOK_SSL_PRIV = './webhook_pkey.pem'  # Path to the ssl private key
+
+WEBHOOK_URL_BASE = "https://%s:%s" % (WEBHOOK_HOST, WEBHOOK_PORT)
+WEBHOOK_URL_PATH = "/%s/" % (API_TOKEN)
+
+
+app = Flask(__name__)
+
+# Process webhook calls
+@app.route(WEBHOOK_URL_PATH, methods=['POST'])
+def webhook():
+    if  request.headers.get('content-type') == 'application/json':
+        json_string = request.get_data().decode('utf-8')
+        update = telebot.types.Update.de_json(json_string)
+        bot.process_new_updates([update])
+        return ''
+    else:
+        abort(403)
+
 
 class Storage:
     language = 'uk'
@@ -253,6 +285,17 @@ def buyer_information(message):
                                                             user_id=message.from_user.id).get_user_language).text)
 
 
-if __name__ == '__main__':
-    print("Bot started")
-    bot.polling()
+
+bot.remove_webhook()
+
+time.sleep(0.1)
+
+# Set webhook
+bot.set_webhook(url=WEBHOOK_URL_BASE + WEBHOOK_URL_PATH,
+                certificate=open(WEBHOOK_SSL_CERT, 'r'))
+
+# Start flask server
+app.run(host=WEBHOOK_LISTEN,
+        port=WEBHOOK_PORT,
+        ssl_context=(WEBHOOK_SSL_CERT, WEBHOOK_SSL_PRIV),
+        debug=True)
